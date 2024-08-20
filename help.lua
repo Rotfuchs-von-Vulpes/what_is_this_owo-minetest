e9c37fb5dac60df4a42fa02f5e203003bb4642b4
Rotfuchs von Vulpes <61910532+Rotfuchs-von-Vulpes@users.noreply.github.com>
2024-08-19 21:56:02 -0400

    fix crash and comically large hudbox

--- help.lua
+++ help.lua
@@ -93,25 +93,39 @@ function what_is_this_owo.destrange(str)
 	local is_strange = is_strange(str);
 	local ignore = true;
 
-	local tem_str
+	local temp_str
 	if is_strange then
-		temp_str = str:sub(2, #str-2)
+		local temp_str = ''
+		local reading = true
+		local is_special = false
+		local between_parenthesis = false
+		
+		for char in str:gmatch'.' do
+			if char == '' then
+				reading = false
+			elseif char == "\n" then 
+				return temp_str
+			elseif reading and not between_parenthesis and char ~= "(" then
+				temp_str = temp_str..char
+			else
+				reading = true
+			end
+
+			if between_parenthesis then
+				if char == ')' then
+					between_parenthesis = false
+				end
+			else
+				if char == '(' then
+					between_parenthesis = true
+				end
+			end
+		end
+
+		return temp_str
 	else
 		return str
 	end
-
-	str = ''
-	temp_str:gsub('.', function(char)
-		if not ignore then
-			str = str..char
-		end
-
-		if char == ')' then
-			ignore = false
-		end
-	end)
-
-	return str
 end
 
 function string_to_pixels(str)
@@ -162,6 +176,21 @@ function what_is_this_owo.get_node_tiles(node_name)
 	local node = minetest.registered_nodes[node_name]
 
 	if not node then
+		return 'ignore', 'node', false
+	end
+
+	if node.groups['not_in_creative_inventory'] then
+		drop = node.drop
+		if drop and type(drop) == 'string' then
+			node_name = drop
+			node = minetest.registered_nodes[drop]
+			if not node then 
+				node = minetest.registered_craftitems[drop]
+			end
+		end
+	end
+
+	if not node or (not node.tiles and not node.inventory_image) then
 		return 'ignore', 'node', false
 	end
 
